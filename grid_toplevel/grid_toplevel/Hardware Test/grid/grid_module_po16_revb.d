011279bf70ff18809ea98fb14900526c2635d7d3
suku (wpc-a) <sukuwc@riseup.net>
2020-04-03 11:15:19 +0200

    SUKU encoder button update RELEASE

--- grid_toplevel/grid_toplevel/Hardware Test/grid/grid_module_po16_revb.d
+++ grid_toplevel/grid_toplevel/Hardware Test/grid/grid_module_po16_revb.d
@@ -204,9 +204,9 @@ grid/grid_module_po16_revb.d grid/grid_module_po16_revb.o: \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-none-eabi\include\alloca.h \
  ../grid/grid_module_bu16_revb.h ../grid/grid_module_pbf4_reva.h \
  ../grid/grid_module_en16_reva.h ../atmel_start_pins.h \
- ../grid/grid_unittest.h ../grid/grid_ain.h ../grid/grid_module.h \
- ../grid/grid_buf.h ../grid/grid_led.h ../grid/grid_protocol.h \
- ../grid/grid_sys.h ../grid/grid_ui.h
+ ../grid/grid_unittest.h ../grid/grid_debug.h ../grid/grid_ain.h \
+ ../grid/grid_module.h ../grid/grid_buf.h ../grid/grid_led.h \
+ ../grid/grid_protocol.h ../grid/grid_sys.h ../grid/grid_ui.h
 
 ../grid/grid_module_po16_revb.h:
 
@@ -751,6 +751,8 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\arm-no
 ../atmel_start_pins.h:
 
 ../grid/grid_unittest.h:
+
+../grid/grid_debug.h:
 
 ../grid/grid_ain.h:
 
