330e41aa4e4400964528edd201ad66af44e1a10c
sukuwc <sukuwc@riseup.net>
2020-12-04 20:59:40 +0100

    SUKU last commit 2/2

---
+++ grid_toplevel/grid_toplevel/grid_toplevel.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,34 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# Release configuration
+CND_ARTIFACT_DIR_Release=dist/Release/production
+CND_ARTIFACT_NAME_Release=grid_toplevel.X.production.hex
+CND_ARTIFACT_PATH_Release=dist/Release/production/grid_toplevel.X.production.hex
+CND_PACKAGE_DIR_Release=${CND_DISTDIR}/Release/package
+CND_PACKAGE_NAME_Release=gridtoplevel.x.tar
+CND_PACKAGE_PATH_Release=${CND_DISTDIR}/Release/package/gridtoplevel.x.tar
+# Debug configuration
+CND_ARTIFACT_DIR_Debug=dist/Debug/production
+CND_ARTIFACT_NAME_Debug=grid_toplevel.X.production.hex
+CND_ARTIFACT_PATH_Debug=dist/Debug/production/grid_toplevel.X.production.hex
+CND_PACKAGE_DIR_Debug=${CND_DISTDIR}/Debug/package
+CND_PACKAGE_NAME_Debug=gridtoplevel.x.tar
+CND_PACKAGE_PATH_Debug=${CND_DISTDIR}/Debug/package/gridtoplevel.x.tar
+# Unittest configuration
+CND_ARTIFACT_DIR_Unittest=dist/Unittest/production
+CND_ARTIFACT_NAME_Unittest=grid_toplevel.X.production.hex
+CND_ARTIFACT_PATH_Unittest=dist/Unittest/production/grid_toplevel.X.production.hex
+CND_PACKAGE_DIR_Unittest=${CND_DISTDIR}/Unittest/package
+CND_PACKAGE_NAME_Unittest=gridtoplevel.x.tar
+CND_PACKAGE_PATH_Unittest=${CND_DISTDIR}/Unittest/package/gridtoplevel.x.tar
+# Nightly configuration
+CND_ARTIFACT_DIR_Nightly=dist/Nightly/production
+CND_ARTIFACT_NAME_Nightly=grid_toplevel.X.production.hex
+CND_ARTIFACT_PATH_Nightly=dist/Nightly/production/grid_toplevel.X.production.hex
+CND_PACKAGE_DIR_Nightly=${CND_DISTDIR}/Nightly/package
+CND_PACKAGE_NAME_Nightly=gridtoplevel.x.tar
+CND_PACKAGE_PATH_Nightly=${CND_DISTDIR}/Nightly/package/gridtoplevel.x.tar
--- grid_toplevel_bak3/grid_toplevel/Release/grid/grid_debug.d
+++
@@ -1,1 +0,0 @@
-grid/grid_debug.d grid/grid_debug.o: ../grid/grid_debug.c
