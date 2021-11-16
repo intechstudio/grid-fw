a23bc0f46004a0f610f657895e77191e595eea9e
sukuwc <sukuwc@riseup.net>
2021-11-16 11:03:39 +0100

    SUKU docker files removed, gitignor updated

--- grid_make/docker_build.sh
+++
@@ -1,1 +0,0 @@
-podman build -t armswdev/arm-tools:bare-metal-compilers -f Dockerfile .
--- grid_make/docker_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-podman run -w /usr/grid_make/gcc -v ./:/usr/grid_make/ --rm -it $* armswdev/arm-tools:bare-metal-compilers
-
