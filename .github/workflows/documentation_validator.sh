edc7374035d75b3b69a23133d2dc4250049065b1
SukuWc <32985513+SukuWc@users.noreply.github.com>
2022-01-04 14:40:13 +0100

    SUKU documentation_validator.sh init

---
+++ .github/workflows/documentation_validator.sh
@@ -0,0 +1,30 @@
+# List all of the Grid LUA API function in text.txt
+touch "test.txt"
+grep -i "GRID_LUA_FNC_.*_human" "grid_protocol_bot.json" > test.txt
+
+# Regexpr to only keep the human readable fnc names
+sed -r -i 's/ *".*"://g' test.txt
+sed -r -i 's/ "//g' test.txt
+sed -r -i 's/",//g' test.txt
+
+# Search function in documentation
+file=$(cat test.txt)
+
+pass=0
+fail=0
+
+for line in $file
+do
+
+    if ls | grep -q -ir "$line" "./grid-documentation"; then ((pass++)); else ((fail++)); fi
+
+done
+
+echo "Coverage: $((100*$pass/($pass+$fail)))%  ($pass Passed, $fail Failed)"
+
+for line in $file
+do
+
+    if ls | grep -q -ir "$line" "./grid-documentation"; then pass=0; else echo "FAILED: $line"; fi
+
+done
