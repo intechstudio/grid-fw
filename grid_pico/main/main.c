5adcd0e529d3dbe12238724e798fbd1078b85e9c
suku <sukuwc@riseup.net>
2022-09-16 15:05:07 +0200

    SUKU GR-3 PIO UART RX Test

--- grid_pico/main/main.c
+++ grid_pico/main/main.c
@@ -7,18 +7,28 @@
 #include "pico/stdlib.h"
 
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
 #include "hardware/pio.h"
 #include "uart_tx.pio.h"
+#include "uart_rx.pio.h"
+
+#include "hardware/uart.h"
 
 int main() 
 {
-    // We're going to use PIO to print "Hello, world!" on the same GPIO which we
-    // normally attach UART0 to.
+
+    // Setup COMMON stuff
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    
+    const uint SERIAL_BAUD = 2000000UL;
+
+
+    // Setup PIO UART TX
     const uint PIN_TX_N = 0;
     const uint PIN_TX_E = 1;
     
-    const uint SERIAL_BAUD = 10000000UL;
-
     PIO pio_N = pio0;
     uint sm_N = 0;
     uint offset_N = pio_add_program(pio_N, &uart_tx_program);
@@ -28,28 +38,37 @@ int main()
     uint sm_E = 1;
     uint offset_E = pio_add_program(pio_E, &uart_tx_program);
     uart_tx_program_init(pio_E, sm_E, offset_E, PIN_TX_E, SERIAL_BAUD);
-
-
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
     
+    // Setup PIO UART RX
+    const uint PIN_RX_N = 3;
+
+    PIO pio = pio1;
+    uint sm = 0;
+    uint offset = pio_add_program(pio, &uart_rx_program);
+    uart_rx_program_init(pio, sm, offset, PIN_RX_N, SERIAL_BAUD);
+
+
+    uint8_t loopcouter = 0;
+
     while (1) 
     {
+        loopcouter++;
+        loopcouter%=10;
 
         gpio_put(LED_PIN, 1);
-        sleep_ms(100);
         gpio_put(LED_PIN, 0);
-        sleep_ms(100);
-        gpio_put(LED_PIN, 1);
-        sleep_ms(100);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(1000);
 
 
-        uart_tx_program_puts(pio_N, sm_N, "U");
+        uart_tx_program_putc(pio_N, sm_N, '0'+loopcouter);
 
-        uart_tx_program_puts(pio_E, sm_E, "F");
+
+        if (uart_rx_program_is_available(pio, sm)){
+            char c = uart_rx_program_getc(pio, sm);
+            uint8_t buffer[10] = {0};
+            sprintf(buffer, "%c", c);
+
+            uart_tx_program_puts(pio_E, sm_E, buffer);
+        }
 
         
     }
--- grid_pico/main/uart_tx.c
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#include "pico/stdlib.h"
-#include "hardware/pio.h"
-#include "uart_tx.pio.h"
