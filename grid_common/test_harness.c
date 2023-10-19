90b642c0a13b005b790de2b1b733517f06234456
suku <sukuwc@riseup.net>
2023-10-19 10:05:52 +0200

    SUKU fuzzing test

---
+++ grid_common/test_harness.c
@@ -0,0 +1,13 @@
+// fuzz_target.cc
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+
+
+  // Simulate a simple condition where the fuzzer sometimes fails.
+  if (Size % 1000 == 0) {
+      // Fail the input.
+      return -1;
+  }
+
+  // Pass the input.
+  return 0;  // Values other than 0 and -1 are reserved for future use.
+}
