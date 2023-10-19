fbb5845e8f7858ce009b5ae7066d25a8778ef681
suku <sukuwc@riseup.net>
2023-10-19 10:34:05 +0200

    SUKU new harness

--- grid_common/test_harness.c
+++ grid_common/test_harness.c
@@ -1,6 +1,7 @@
 // fuzz_target.cc
 #include <stdint.h>
 #include <stddef.h>
+#include <stdio.h>
 
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
@@ -8,7 +9,25 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   // Simulate a simple condition where the fuzzer sometimes fails.
   if (Size % 1000 == 0) {
       // Fail the input.
+      printf("Size error\r\n");
       return -1;
+  }
+
+  // Initialize the random number generator.
+  srand(42);  // You can choose any seed value.
+
+  // Simulate a condition where the fuzzer sometimes fails randomly.
+  if (rand() % 2 == 0) {
+      // Fail the input.
+      printf("Random error\r\n");
+      return -1;
+  }
+
+  // Pass the input.
+
+  if (Data[0] == 0){
+    printf("Data error\r\n");
+    return -1;
   }
 
   // Pass the input.
