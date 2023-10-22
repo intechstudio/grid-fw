14b27fed215a18b1beb613ab0c42c110fb78001a
suku <sukuwc@riseup.net>
2023-10-23 00:39:28 +0200

    SUKU test

--- grid_common/test_harness.c
+++ grid_common/test_harness.c
@@ -9,11 +9,14 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
 
   // Pass the input.
 
+  if (size<10){
+    return 0;
+  }
+
   if (size > 0 && data[0] == 'H')
     if (size > 1 && data[1] == 'I')
        if (size > 2 && data[2] == '!')
        __builtin_trap();
-  return 0;
 
   // Pass the input.
   return 0;  // Values other than 0 and -1 are reserved for future use.
