87f1f6caaee811e1d2c695993c197303fa24f436
suku <sukuwc@riseup.net>
2023-10-22 18:44:11 +0200

    SUKU sync

--- grid_common/test_harness.c
+++ grid_common/test_harness.c
@@ -6,27 +6,11 @@
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
 
-  // Simulate a simple condition where the fuzzer sometimes fails.
-  if (Size % 1000 == 0) {
-      // Fail the input.
-      printf("Size error\r\n");
-      return -1;
-  }
-
-  // Initialize the random number generator.
-  srand(42);  // You can choose any seed value.
-
-  // Simulate a condition where the fuzzer sometimes fails randomly.
-  if (rand() % 2 == 0) {
-      // Fail the input.
-      printf("Random error\r\n");
-      return -1;
-  }
 
   // Pass the input.
 
-  if (Data[0] == 0){
-    printf("Data error\r\n");
+  if (Data[10] == 0){
+    printf("Data error %s\r\n", Data);
     return -1;
   }
 
