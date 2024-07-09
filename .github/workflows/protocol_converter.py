7c6ea9e79cdaa5c2fbe8030c1183bbf87596b3f3
suku <sukuwc@riseup.net>
2024-07-09 15:33:44 +0200

    SUKU package.json hack removed

--- .github/workflows/protocol_converter.py
+++ .github/workflows/protocol_converter.py
@@ -183,8 +183,8 @@ if __name__ == '__main__':
 
     input_files_comma_separated = sys.argv[1] if len(sys.argv) > 1 else 'grid_common/grid_protocol.h'
     output_file = sys.argv[2] if len(sys.argv) > 2 else 'out.json'
-    package_file = sys.argv[3] if len(sys.argv) > 3 else 'package.json'
-    constlist_file = sys.argv[4] if len(sys.argv) > 4 else 'lists.py'
+    constlist_file = sys.argv[3] if len(sys.argv) > 3 else 'lists.py'
+    package_file = sys.argv[4] if len(sys.argv) > 4 else 'package.json'
 
     input_files = input_files_comma_separated.split(',')
 
@@ -200,7 +200,8 @@ if __name__ == '__main__':
     character_lookup = create_character_lookup(input_string)
     # print(json.dumps(character_lookup, indent=2))
 
-    if len(sys.argv)>3:
+    generate_lists_py(constlist_file, character_lookup, class_databse)
+    
+    if len(sys.argv)>4:
         generate_package_json(output_file, package_file)
 
-    generate_lists_py(constlist_file, character_lookup, class_databse)
