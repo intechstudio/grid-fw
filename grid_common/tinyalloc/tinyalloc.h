b02e819dadf68a319c50a18c4c1493e49d2d64df
suku <sukuwc@riseup.net>
2025-02-14 23:45:35 +0100

    SUKU format fixed

--- grid_common/tinyalloc/tinyalloc.h
+++ grid_common/tinyalloc/tinyalloc.h
@@ -5,10 +5,10 @@ extern "C" {
 #include <stdbool.h>
 #include <stddef.h>
 
-bool ta_init(const void *base, const void *limit, const size_t heap_blocks, const size_t split_thresh, const size_t alignment);
-void *ta_alloc(size_t num);
-void *ta_calloc(size_t num, size_t size);
-bool ta_free(void *ptr);
+bool ta_init(const void* base, const void* limit, const size_t heap_blocks, const size_t split_thresh, const size_t alignment);
+void* ta_alloc(size_t num);
+void* ta_calloc(size_t num, size_t size);
+bool ta_free(void* ptr);
 
 size_t ta_num_free();
 size_t ta_num_used();
