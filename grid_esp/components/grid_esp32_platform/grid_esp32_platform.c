3db6d2d8f40173b9321fa70a1f9d9f4d705fc9a1
benblaise <benblaise-intech@proton.me>
2025-02-18 11:54:38 +0100

    BENB fix warnings in grid_esp32_adc and grid_esp32_platform

--- grid_esp/components/grid_esp32_platform/grid_esp32_platform.c
+++ grid_esp/components/grid_esp32_platform/grid_esp32_platform.c
@@ -35,10 +35,10 @@ void* grid_platform_allocate_volatile(size_t size) {
   return handle;
 }
 
-uint64_t grid_platform_rtc_get_micros(void) { return esp_timer_get_time(); }
+uint64_t IRAM_ATTR grid_platform_rtc_get_micros(void) { return esp_timer_get_time(); }
 
-uint64_t grid_platform_rtc_get_elapsed_time(uint64_t told) { return grid_platform_rtc_get_micros() - told; }
+uint64_t IRAM_ATTR grid_platform_rtc_get_elapsed_time(uint64_t told) { return grid_platform_rtc_get_micros() - told; }
 
-uint32_t grid_platform_get_cycles() { return esp_cpu_get_cycle_count(); }
+uint32_t IRAM_ATTR grid_platform_get_cycles() { return esp_cpu_get_cycle_count(); }
 
-uint32_t grid_platform_get_cycles_per_us() { return 240; }
+uint32_t IRAM_ATTR grid_platform_get_cycles_per_us() { return 240; }
