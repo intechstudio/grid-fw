ed8a8f7895f3432ed3b6ccd3ae70af4b7b61899d
benblaise <benblaise-intech@proton.me>
2025-02-20 14:14:50 +0100

    BENB fix warnings in grid_decode.c

--- grid_common/grid_decode.c
+++ grid_common/grid_decode.c
@@ -3,7 +3,7 @@
 #include "grid_ui_encoder.h"
 #include "grid_ui_potmeter.h"
 
-static enum GRID_DESTINATION {
+enum GRID_DESTINATION {
 
   GRID_DESTINATION_IS_ME = 1,
   GRID_DESTINATION_IS_GLOBAL = 2,
@@ -14,9 +14,9 @@ static uint8_t grid_check_destination(char* header, uint8_t target_destination_b
 
   uint8_t error = 0;
 
-  uint8_t position_is_me = 0;
-  uint8_t position_is_global = 0;
-  uint8_t position_is_local = 0;
+  // uint8_t position_is_me = 0;
+  // uint8_t position_is_global = 0;
+  // uint8_t position_is_local = 0;
 
   uint8_t dx = grid_str_get_parameter(header, GRID_BRC_DX_offset, GRID_BRC_DX_length, &error);
   uint8_t dy = grid_str_get_parameter(header, GRID_BRC_DY_offset, GRID_BRC_DY_length, &error);
@@ -196,7 +196,7 @@ uint8_t grid_decode_mousemove_to_usb(char* header, char* chunk) {
   uint8_t position_raw = grid_str_get_parameter(chunk, GRID_CLASS_HIDMOUSEMOVE_POSITION_offset, GRID_CLASS_HIDMOUSEMOVE_POSITION_length, &error);
   uint8_t axis = grid_str_get_parameter(chunk, GRID_CLASS_HIDMOUSEMOVE_AXIS_offset, GRID_CLASS_HIDMOUSEMOVE_AXIS_length, &error);
 
-  int8_t position = position_raw - 128;
+  // int8_t position = position_raw - 128;
 
   struct grid_usb_keyboard_event_desc key;
 
@@ -513,7 +513,7 @@ uint8_t grid_decode_imediate_to_ui(char* header, char* chunk) {
     struct grid_msg_packet message_global;
     grid_msg_packet_init(&grid_msg_state, &message_global, GRID_PARAMETER_DEFAULT_POSITION, GRID_PARAMETER_DEFAULT_POSITION);
     char payload_global[GRID_PARAMETER_PACKET_maxlength] = {0};
-    uint32_t offset_global = 0;
+    // uint32_t offset_global = 0;
 
     strcat(payload_global, grid_lua_get_output_string(&grid_lua_state));
     grid_lua_clear_stdo(&grid_lua_state);
@@ -570,7 +570,7 @@ uint8_t grid_decode_heartbeat_to_ui(char* header, char* chunk) {
       // printf("254\r\n");
     }
 
-    uint8_t ui_report_valid = 0;
+    // uint8_t ui_report_valid = 0;
 
     if (editor_connected_now) {
 
@@ -605,7 +605,7 @@ uint8_t grid_decode_heartbeat_to_ui(char* header, char* chunk) {
 
         // grid_led_state.led_lowlevel_changed[j] = 0;
 
-        ui_report_valid = 1;
+        // ui_report_valid = 1;
 
         sprintf(&response_payload[len], "%02x%02x", element_num, element_value);
         len += strlen(&response_payload[len]);
@@ -629,7 +629,7 @@ uint8_t grid_decode_heartbeat_to_ui(char* header, char* chunk) {
     // report stringnames
     if (editor_connected_now) {
 
-      uint16_t report_length = 0;
+      // uint16_t report_length = 0;
       struct grid_msg_packet response;
 
       grid_msg_packet_init(&grid_msg_state, &response, GRID_PARAMETER_GLOBAL_POSITION, GRID_PARAMETER_GLOBAL_POSITION);
@@ -637,9 +637,9 @@ uint8_t grid_decode_heartbeat_to_ui(char* header, char* chunk) {
       // -1 to exclude system element
       for (uint8_t j = 0; j < grid_ui_state.element_list_length - 1; j++) {
 
-        struct grid_ui_element* ele = &grid_ui_state.element_list[j];
+        // struct grid_ui_element* ele = &grid_ui_state.element_list[j];
 
-        uint8_t number = j;
+        // uint8_t number = j;
         char command[26] = {0};
 
         sprintf(command, "gens(%d,ele[%d]:gen())", j, j);
@@ -761,10 +761,10 @@ uint8_t grid_decode_uptime_to_ui(char* header, char* chunk) {
 
     grid_msg_packet_body_set_parameter(&response, 0, GRID_CLASS_UPTIME_UPTIME_offset, GRID_CLASS_UPTIME_UPTIME_length, uptime);
 
-    uint32_t milliseconds = uptime / MS_TO_US % 1000;
-    uint32_t seconds = uptime / MS_TO_US / 1000 % 60;
-    uint32_t minutes = uptime / MS_TO_US / 1000 / 60 % 60;
-    uint32_t hours = uptime / MS_TO_US / 1000 / 60 / 60 % 60;
+    // uint32_t milliseconds = uptime / MS_TO_US % 1000;
+    // uint32_t seconds = uptime / MS_TO_US / 1000 % 60;
+    // uint32_t minutes = uptime / MS_TO_US / 1000 / 60 % 60;
+    // uint32_t hours = uptime / MS_TO_US / 1000 / 60 / 60 % 60;
 
     grid_msg_packet_close(&grid_msg_state, &response);
     grid_port_packet_send_everywhere(&response);
@@ -858,8 +858,8 @@ uint8_t grid_decode_pagediscard_to_ui(char* header, char* chunk) {
 
 uint8_t grid_decode_pagestore_to_ui(char* header, char* chunk) {
 
-  uint8_t dx = grid_str_get_parameter(header, GRID_BRC_DX_offset, GRID_BRC_DX_length, NULL);
-  uint8_t dy = grid_str_get_parameter(header, GRID_BRC_DY_offset, GRID_BRC_DY_length, NULL);
+  // uint8_t dx = grid_str_get_parameter(header, GRID_BRC_DX_offset, GRID_BRC_DX_length, NULL);
+  // uint8_t dy = grid_str_get_parameter(header, GRID_BRC_DY_offset, GRID_BRC_DY_length, NULL);
 
   if (grid_check_destination(header, GRID_DESTINATION_IS_ME | GRID_DESTINATION_IS_GLOBAL) == false) {
     return 1;
@@ -1019,9 +1019,9 @@ uint8_t grid_decode_config_to_ui(char* header, char* chunk) {
     grid_usb_keyboard_disable(&grid_usb_keyboard_state);
     // grid_port_debug_print_text("Disabling KB");
 
-    uint8_t vmajor = grid_str_get_parameter(chunk, GRID_CLASS_CONFIG_VERSIONMAJOR_offset, GRID_CLASS_CONFIG_VERSIONMAJOR_length, NULL);
-    uint8_t vminor = grid_str_get_parameter(chunk, GRID_CLASS_CONFIG_VERSIONMINOR_offset, GRID_CLASS_CONFIG_VERSIONMINOR_length, NULL);
-    uint8_t vpatch = grid_str_get_parameter(chunk, GRID_CLASS_CONFIG_VERSIONPATCH_offset, GRID_CLASS_CONFIG_VERSIONPATCH_length, NULL);
+    // uint8_t vmajor = grid_str_get_parameter(chunk, GRID_CLASS_CONFIG_VERSIONMAJOR_offset, GRID_CLASS_CONFIG_VERSIONMAJOR_length, NULL);
+    // uint8_t vminor = grid_str_get_parameter(chunk, GRID_CLASS_CONFIG_VERSIONMINOR_offset, GRID_CLASS_CONFIG_VERSIONMINOR_length, NULL);
+    // uint8_t vpatch = grid_str_get_parameter(chunk, GRID_CLASS_CONFIG_VERSIONPATCH_offset, GRID_CLASS_CONFIG_VERSIONPATCH_length, NULL);
 
     uint8_t pagenumber = grid_str_get_parameter(chunk, GRID_CLASS_CONFIG_PAGENUMBER_offset, GRID_CLASS_CONFIG_PAGENUMBER_length, NULL);
     uint8_t elementnumber = grid_str_get_parameter(chunk, GRID_CLASS_CONFIG_ELEMENTNUMBER_offset, GRID_CLASS_CONFIG_ELEMENTNUMBER_length, NULL);
@@ -1218,7 +1218,7 @@ struct grid_decoder_collection grid_decoder_to_ui[] = {{GRID_CLASS_PAGEACTIVE_co
                                                        {GRID_CLASS_NVMDEFRAG_code, grid_decode_nvmdefrag_to_ui},
                                                        {GRID_CLASS_CONFIG_code, grid_decode_config_to_ui},
                                                        {GRID_CLASS_HIDKEYSTATUS_code, grid_decode_hidkeystatus_to_ui},
-                                                       {NULL, NULL}};
+                                                       {0, NULL}};
 
 struct grid_decoder_collection* grid_decoder_to_ui_reference = grid_decoder_to_ui;
 
@@ -1229,6 +1229,6 @@ struct grid_decoder_collection grid_decoder_to_usb[] = {{GRID_CLASS_MIDI_code, g
                                                         {GRID_CLASS_HIDGAMEPADBUTTON_code, grid_decode_gamepadbutton_to_usb},
                                                         {GRID_CLASS_HIDGAMEPADMOVE_code, grid_decode_gamepadmove_to_usb},
                                                         {GRID_CLASS_HIDKEYBOARD_code, grid_decode_keyboard_to_usb},
-                                                        {NULL, NULL}};
+                                                        {0, NULL}};
 
 struct grid_decoder_collection* grid_decoder_to_usb_reference = grid_decoder_to_usb;
