9ed5756831cafa115f2f1ba401b5db111e3ef3d7
benblaise <benblaise-intech@proton.me>
2025-05-12 13:08:00 +0200

    BENB update protocol version

--- grid_common/grid_protocol.h
+++ grid_common/grid_protocol.h
@@ -17,7 +17,7 @@
 
 #define GRID_PROTOCOL_VERSION_MAJOR 1
 #define GRID_PROTOCOL_VERSION_MINOR 3
-#define GRID_PROTOCOL_VERSION_PATCH 2
+#define GRID_PROTOCOL_VERSION_PATCH 4
 
 // must not change because it would break profiles
 #define GRID_PARAMETER_ELEMENT_SYSTEM 0
