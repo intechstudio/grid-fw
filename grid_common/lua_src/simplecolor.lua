3a32b0c4257f3300de61163d9174732d88cea728
sukuwc <sukuwc@riseup.net>
2025-05-26 17:21:59 +0200

    SUKU init_endless_nosegment_color implemented

--- grid_common/lua_src/simplecolor.lua
+++ grid_common/lua_src/simplecolor.lua
@@ -34,6 +34,31 @@ init_endless_color = function(self)
     end
 end
 
+init_endless_nosegment_color = function(self)
+    self.glp = function(self, l, v)
+        for i = 0, 4, 1 do
+            local ln = self:lix() + i * self:lof()
+            local min, max, value = self:epmi(), self:epma(), self:epva()
+            if l == 1 then
+                min, max, value = self:bmi(), self:bma(), self:bva()
+            end
+            local int = gmaps(value, min, max, 0, 255) // 1
+            glp(ln, l, int)
+        end
+    end
+    self.glc = function(self, l, c)
+        local up = table.unpack
+        local x, y, z = cp(c)
+        for i = 0, 4, 1 do
+            local ln = self:ind() + i * self:lof()
+            gln(ln, l, up(x))
+            gld(ln, l, up(y))
+            glx(ln, l, up(z))
+        end
+    end
+end
+
+
 init_element_color = function(self)
     self.glp = function(self, l, v)
         glp(self:ind(), l, -1)
