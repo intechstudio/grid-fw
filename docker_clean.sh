fb021fefc2c00223ce95aa56d5adb94237673606
benblaise <benblaise-intech@proton.me>
2026-03-12 15:54:01 +0100

    BENB update Dockerfile and scripts

--- docker_clean.sh
+++ docker_clean.sh
@@ -1,3 +1,28 @@
-#!/bin/bash
-CONTAINER_TOOL=$(command -v docker || command -v podman)
-$CONTAINER_TOOL rm -f $($CONTAINER_TOOL ps -a -q --filter ancestor=idf-pico-merged) 2>/dev/null; $CONTAINER_TOOL rmi idf-pico-merged
+#!/bin/sh
+
+if command -v docker 1> /dev/null; then
+	CONTAINER_CMD="docker"
+elif command -v podman 1> /dev/null; then
+	CONTAINER_CMD="podman"
+else
+	echo "Neither docker nor podman found. Please install either to proceed."
+	exit 1
+fi
+
+IMAGE=${IMAGE:-grid-fw-build}
+
+# Check that the image exists
+if [ -z "$(sudo docker images -q "$IMAGE")" ] ; then
+	echo "Image does not seem to exist."
+	exit 1
+fi
+
+# Remove containers that share the image as an ancestor
+CONTAINER_IDS="$($CONTAINER_CMD ps -a -q --filter ancestor="$IMAGE")"
+if [ -n "$CONTAINER_IDS" ] ; then
+	echo "$CONTAINER_IDS"
+	$CONTAINER_CMD rm -f $CONTAINER_IDS
+fi
+
+# Remove image
+$CONTAINER_CMD rmi "$IMAGE"
