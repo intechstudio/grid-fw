3a5cdf802979f83a2115a759060420519ed0fd8c
sukuwc <sukuwc@riseup.net>
2026-06-15 10:54:20 +0200

    remove long comment, TAG, ESP_LOGI, unused includes from grid_esp32_usb.c

--- esp32s3/components/grid_esp32_usb/grid_esp32_usb.c
+++ esp32s3/components/grid_esp32_usb/grid_esp32_usb.c
@@ -1,26 +1,13 @@
-/*
- * SPDX-FileCopyrightText: 2021-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Unlicense OR CC0-1.0
- */
-
 #include "tusb.h"
 
-#include "esp_check.h"
-#include "esp_log.h"
-
+#include "esp_err.h"
 #include "esp_private/usb_phy.h"
 #include "grid_esp32_usb.h"
 #include "grid_platform.h"
 #include "grid_usb.h"
 
-static const char* TAG = "USB";
-
-// ========================= USB Initialization =============================== //
-
 void grid_esp32_usb_init(void) {
 
-  // 1. Configure the on-chip USB PHY for full-speed OTG device mode.
   usb_phy_handle_t phy_hdl;
   usb_phy_config_t phy_conf = {
       .controller = USB_PHY_CTRL_OTG,
@@ -39,8 +26,6 @@ void grid_esp32_usb_init(void) {
     serial[i * 2] = hex[(b[i] >> 4) & 0xf];
     serial[i * 2 + 1] = hex[b[i] & 0xf];
   }
-  serial[12] = '\0'; // 48-bit MAC address
+  serial[12] = '\0';
   grid_usb_init(0x303a, 0x8123, serial);
-
-  ESP_LOGI(TAG, "USB initialized");
 }
