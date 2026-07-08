6cb475ecce029d2436ece905872d4a02865a1b30
benblaise <benblaise-intech@proton.me>
2026-07-08 12:24:09 +0200

    BENB remove unused ui element callbacks

--- common/src/c/grid_ui_encoder.c
+++ common/src/c/grid_ui_encoder.c
@@ -51,6 +51,29 @@ void grid_ui_encoder_state_init(struct grid_ui_encoder_state* state, uint8_t det
   grid_ui_button_state_init(&state->button, 1, GRID_BUTTON_THRESHOLD, 0.0);
 }
 
+void grid_ui_element_encoder_event_clear_cb(struct grid_ui_event* eve) {
+
+  int32_t* template_parameter_list = eve->parent->template_parameter_list;
+
+  template_parameter_list[GRID_LUA_FNC_E_ENCODER_STATE_index] = 64;
+
+  if (template_parameter_list[GRID_LUA_FNC_E_ENCODER_MODE_index] == 1) { // relative
+
+    int32_t min = template_parameter_list[GRID_LUA_FNC_E_ENCODER_MIN_index];
+    int32_t max = template_parameter_list[GRID_LUA_FNC_E_ENCODER_MAX_index];
+
+    int32_t val = ((max + 1) + min) / 2;
+
+    template_parameter_list[GRID_LUA_FNC_E_ENCODER_VALUE_index] = val;
+
+  } else if (template_parameter_list[GRID_LUA_FNC_E_ENCODER_MODE_index] == 2) {
+
+    int32_t min = template_parameter_list[GRID_LUA_FNC_E_ENCODER_MIN_index];
+
+    template_parameter_list[GRID_LUA_FNC_E_ENCODER_VALUE_index] = min;
+  }
+}
+
 void grid_ui_element_encoder_init(struct grid_ui_element* ele) {
 
   ele->type = GRID_PARAMETER_ELEMENT_ENCODER;
@@ -81,7 +104,7 @@ void grid_ui_element_encoder_init(struct grid_ui_element* ele) {
   ele->template_parameter_index_max[1] = GRID_LUA_FNC_E_ENCODER_MAX_index;
 
   ele->event_clear_cb = &grid_ui_element_encoder_event_clear_cb;
-  ele->page_change_cb = &grid_ui_element_encoder_page_change_cb;
+  ele->page_change_cb = NULL;
 }
 
 void grid_ui_element_encoder_template_parameter_init(struct grid_ui_template_buffer* buf) {
@@ -109,44 +132,6 @@ void grid_ui_element_encoder_template_parameter_init(struct grid_ui_template_buf
   template_parameter_list[GRID_LUA_FNC_E_ENCODER_STATE_index] = 64;
   template_parameter_list[GRID_LUA_FNC_E_ENCODER_VELOCITY_index] = 50;
   template_parameter_list[GRID_LUA_FNC_E_ENCODER_SENSITIVITY_index] = 100;
-}
-
-void grid_ui_element_encoder_event_clear_cb(struct grid_ui_event* eve) {
-
-  int32_t* template_parameter_list = eve->parent->template_parameter_list;
-
-  template_parameter_list[GRID_LUA_FNC_E_ENCODER_STATE_index] = 64;
-
-  if (template_parameter_list[GRID_LUA_FNC_E_ENCODER_MODE_index] == 1) { // relative
-
-    int32_t min = template_parameter_list[GRID_LUA_FNC_E_ENCODER_MIN_index];
-    int32_t max = template_parameter_list[GRID_LUA_FNC_E_ENCODER_MAX_index];
-
-    int32_t val = ((max + 1) + min) / 2;
-
-    template_parameter_list[GRID_LUA_FNC_E_ENCODER_VALUE_index] = val;
-
-  } else if (template_parameter_list[GRID_LUA_FNC_E_ENCODER_MODE_index] == 2) {
-
-    int32_t min = template_parameter_list[GRID_LUA_FNC_E_ENCODER_MIN_index];
-
-    template_parameter_list[GRID_LUA_FNC_E_ENCODER_VALUE_index] = min;
-  }
-}
-
-void grid_ui_element_encoder_page_change_cb(struct grid_ui_element* ele, uint8_t page_old, uint8_t page_new) {
-
-  // for (uint8_t i = 0; i<16; i++)
-  // {
-
-  // 	struct grid_ui_event* eve = NULL;
-
-  // 	eve = grid_ui_event_find(&grid_ui_state.element_list[i],
-  // GRID_PARAMETER_EVENT_INIT); 	grid_ui_event_trigger_local(eve);
-
-  // 	eve = grid_ui_event_find(&grid_ui_state.element_list[i],
-  // GRID_PARAMETER_EVENT_ENCODER); 	grid_ui_event_trigger_local(eve);
-  // }
 }
 
 #define SIGN(x) (((x) > 0) - ((x) < 0))
